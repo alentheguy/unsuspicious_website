835c883b6d05d30f52a05011ad3d2a1b1519b6d0
alentheguy <alvinw0231@gmail.com>
2026-02-04 18:01:18 -0500

    final?

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -2,17 +2,23 @@
   <div class="video-page">
     <h1>Fullscreen and watch with sound pls :3</h1>
 
-    <video controls class="valentine-video">
-      <source
-        src="https://drive.google.com/uc?export=download&id=1i6ALi3C3xydGOJNxgFHg228H3xJj9iFU"
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video>
     <iframe
-      src="https://drive.google.com/file/d/1i6ALi3C3xydGOJNxgFHg228H3xJj9iFU/preview"
-      width="640"
-      height="480"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/beefvJSzt2s?si=hsLhBEIG0NfmSbRj"
+      title="YouTube video player"
+      frameborder="0"
+      allow="
+        accelerometer;
+        autoplay;
+        clipboard-write;
+        encrypted-media;
+        gyroscope;
+        picture-in-picture;
+        web-share;
+      "
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
     ></iframe>
   </div>
 </template>
