3ef2696bc0c9981838e55f3310b082c2a5220f3b
alentheguy <alvinw0231@gmail.com>
2026-02-04 16:52:25 -0500

    fds

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -3,7 +3,10 @@
     <h1>Fullscreen and watch with sound pls :3</h1>
 
     <video controls class="valentine-video">
-      <source src="@/assets/ValentinesAsk2026.mp4" type="video/mp4" />
+      <source
+        src="https://drive.google.com/file/d/1i6ALi3C3xydGOJNxgFHg228H3xJj9iFU/view?usp=sharing"
+        type="video/mp4"
+      />
       Your browser does not support the video tag.
     </video>
   </div>
